b202de3119249211ea98289781b0522b334d247a
Onkar Sawant <onkarsawant637@gmail.com>
2026-05-08 00:14:22 +0530

    Added Palindrome Java Code

---
+++ src/com/string/basic/questions/Palindrome.java
@@ -0,0 +1,28 @@
+package com.string.basic.questions;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String str="madam";
+		if(checkPalindrome(str)) {
+			System.out.println("Palindrome");
+		}else {
+			System.out.println("Not Palindrome");
+		}
+	}
+	
+	public static boolean checkPalindrome(String str) {
+		int start=0;
+		int end=str.length()-1;
+		
+		while(start<end) {
+			if(str.charAt(start)!=str.charAt(end)) {
+				return false;
+			}
+			start++;
+			end--;
+		}
+		return true;
+	}
+
+}
